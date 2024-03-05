91b50ae6cf89c4c96bd171cf3a54cbeaa6611efd
userwhz <whz5874@163.com>
2024-03-05 19:08:04 +0800

    GeeORM

---
+++ mysql/gee-orm/demo/demo.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+func main() {
+	db, _ := sql.Open("sqlite3", "gee.db")
+	defer func() {
+		db.Close()
+	}()
+	_, _ = db.Exec("DROP TABLE IF EXISTS User;")
+	_, _ = db.Exec("CREATE TABLE User(Name text);")
+	result, err := db.Exec("INSERT INTO User('Name') values (?), (?)", "ww", "xx")
+	if err == nil {
+		affected, _ := result.RowsAffected()
+		log.Println(affected)
+	}
+	row := db.QueryRow("SELECT Name FROM User LIMIT 1")
+	var name string
+	if err := row.Scan(&name); err == nil {
+		log.Printf(name)
+	}
+}
---
+++ mysql/gee-orm/readme.md
@@ -0,0 +1,2 @@
+github https://github.com/geektutu/7days-golang/tree/master/gee-orm
+资料 https://geektutu.com/post/geeorm.html
